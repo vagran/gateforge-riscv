6da9fc3b0a183a520b5f30ead565fde38292f441
Artyom Lebedev <vagran.ast@gmail.com>
2025-03-26 23:12:39 +0200

    Spigot test passes.

---
+++ test/firmware/main_spigot.zig
@@ -0,0 +1,62 @@
+// These symbols come from the linker script
+extern var _data: u32;
+extern const _edata: u32;
+extern var _bss: u32;
+extern const _ebss: u32;
+extern const _sstack: u32;
+
+const std = @import("std");
+
+const HostIo = extern struct {
+    panic: [4]u8,
+    outData: u32,
+    inData: u64,
+    outString: [128]u8
+};
+
+const hostIo: *volatile HostIo = @ptrFromInt(0x800);
+
+export fn _resetHandler() linksection(".reset") noreturn {
+    // Set stack pointer
+    asm volatile ("la sp, _sstack");
+    _start();
+}
+
+export fn _start() noreturn {
+    // Clear the BSS
+    const bss: [*]u8 = @ptrCast(&_bss);
+    const bss_size = @intFromPtr(&_ebss) - @intFromPtr(&_bss);
+    for (bss[0..bss_size]) |*b| b.* = 0;
+
+    @memset(&hostIo.panic, 0);
+
+    main() catch unreachable;
+
+    asm volatile ("ebreak");
+    while (true) {}
+}
+
+pub const panic = std.debug.FullPanic(_panic);
+
+fn _panic(msg: []const u8, first_trace_addr: ?usize) noreturn {
+    hostIo.panic = "PNIC";
+    _ = msg;
+    _ = first_trace_addr;
+    asm volatile ("ebreak");
+    while (true) {}
+}
+
+var heap: [0x4000]u8 = undefined;
+
+const UmmAllocator = @import("umm.zig").UmmAllocator;
+
+const spigot_pi = @import("Spigot.zig").spigot_pi;
+
+
+fn main() !void {
+    const Allocator = UmmAllocator(.{});
+    var allocInst = try Allocator.init(&heap);
+    defer _ = allocInst.deinit();
+    const allocator = allocInst.allocator();
+    try spigot_pi(@volatileCast(&hostIo.outString)[0..10], allocator);
+}
--- test/test_emulated.py
+++ test/test_emulated.py
@@ -77,6 +77,7 @@ class Memory:
 class TestBase(unittest.TestCase):
     hasCompressedIsa = False
     result: CompileResult
+    enableVcd = True
 
 
     @classmethod
@@ -100,6 +101,7 @@ class TestBase(unittest.TestCase):
         self.mem = Memory(self.memSize)
         self.clk = 0
         self.sim.Eval()
+        self.enableVcd = True
         self.sim.OpenVcd(workspaceDir / "test.vcd")
 
 
@@ -109,7 +111,8 @@ class TestBase(unittest.TestCase):
         self.mem.HandleSim(self.ports)
         self.sim.TimeInc(1)
         self.sim.Eval()
-        self.sim.DumpVcd()
+        if self.enableVcd:
+            self.sim.DumpVcd()
 
 
     def Reset(self):
@@ -1018,6 +1021,39 @@ def hash(x1, x2, seed):
     return h
 
 
+Pi = """
+ 31415926535 8979323846 2643383279 5028841971 6939937510
+  5820974944 5923078164 0628620899 8628034825 3421170679
+  8214808651 3282306647 0938446095 5058223172 5359408128
+  4811174502 8410270193 8521105559 6446229489 5493038196
+  4428810975 6659334461 2847564823 3786783165 2712019091
+  4564856692 3460348610 4543266482 1339360726 0249141273
+  7245870066 0631558817 4881520920 9628292540 9171536436
+  7892590360 0113305305 4882046652 1384146951 9415116094
+  3305727036 5759591953 0921861173 8193261179 3105118548
+  0744623799 6274956735 1885752724 8912279381 8301194912
+
+  9833673362 4406566430 8602139494 6395224737 1907021798
+  6094370277 0539217176 2931767523 8467481846 7669405132
+  0005681271 4526356082 7785771342 7577896091 7363717872
+  1468440901 2249534301 4654958537 1050792279 6892589235
+  4201995611 2129021960 8640344181 5981362977 4771309960
+  5187072113 4999999837 2978049951 0597317328 1609631859
+  5024459455 3469083026 4252230825 3344685035 2619311881
+  7101000313 7838752886 5875332083 8142061717 7669147303
+  5982534904 2875546873 1159562863 8823537875 9375195778
+  1857780532 1712268066 1300192787 6611195909 2164201989
+"""
+
+def ParsePiDigits(n):
+    result = []
+    while n > 0:
+        for c in Pi:
+            if c.isdigit():
+                result.append(ord(c) - ord("0"))
+                n -= 1
+    return result
+
 @unittest.skipIf(disableVerilatorTests, "Verilator")
 class TestWithFirmware(TestBase):
     baseDir = Path(__file__).parent
@@ -1061,6 +1097,8 @@ class TestWithFirmware(TestBase):
 
 
     def test_spigot(self):
+        self.enableVcd = False
+
         with open(self.baseDir / "firmware" / "zig-out" / "bin" / "firmware_spigot.bin", mode="rb") as f:
             fw = f.read()
         self.mem.WriteBytes(0, fw)
@@ -1072,4 +1110,7 @@ class TestWithFirmware(TestBase):
         self.WaitEbreak()
 
         self.assertEqual(0, self.mem.ReadWord(testAddr))
-        print(self.mem.ReadBytes(testAddr, 32))
+        digits = self.mem.ReadBytes(testAddr + 16, 10)
+        expected = ParsePiDigits(10)
+        for digit, expectedDigit in zip(digits, expected):
+            self.assertEqual(digit, expectedDigit)
